854bc3613969086f1eb65e6b9347c94d354abc22
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-01-14 12:12:39 +0100

    Day 1 ex 2.

---
+++ day_1_pgg/ex_2.py
@@ -0,0 +1,18 @@
+"""
+Write a program that calculates the cost of travel from city A to city B
+based on the number of kilometers given by the user, the price of fuel
+and fuel consumption. Ask the user for the cities names, distance, fueal price
+and fuel consumption.
+
+Use a proportion formula to calculate this:
+cost = distance * fuel_consumption / 100.0 * price
+
+Sample program input:
+City A: Warsaw
+City B: Gdańsk
+Distance Warsaw-Gdansk: 420
+Fuel price: 4.55
+Fuel consumption per 100 km: 5.5
+Sample program output:
+The cost of the Warsaw-Gdańsk journey is 105 PLN
+"""
