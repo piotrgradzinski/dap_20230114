1caed6680a5e84efd0fb8bf9b4a0bd578c790ebd
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-01-15 11:15:39 +0100

    Day 2 - intro if.

---
+++ day_2_pgg/ex_3.py
@@ -0,0 +1,5 @@
+"""
+Write a program that calculates the average temperature
+for a given week based on temperatures entered by the user.
+"""
+
---
+++ day_2_pgg/intro.py
@@ -0,0 +1,11 @@
+# Loops
+
+# while loop
+
+i = 0
+while i < 10:
+    print(i)
+    i += 2
+
+print('After while loop')
+
