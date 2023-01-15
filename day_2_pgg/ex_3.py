24104d1ebbd6fea4fba5c7a818dbdfea937539a9
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-01-15 11:20:59 +0100

    Day 2 - intro if.

--- day_2_pgg/ex_3.py
+++ day_2_pgg/ex_3.py
@@ -1,5 +1,30 @@
 """
 Write a program that calculates the average temperature
 for a given week based on temperatures entered by the user.
+
+average = sum of temperatures / 7
+
+before while loop:
+    define 2 variables:
+        - storing current day number, for which we are getting data, start with 1
+        - temperature sum variable, start with 0
+while:
+    condition: day number should less than or equal to 7
+    body of the while loop:
+        - get the temp from the user and convert to int and add (+=) to temperature sum variable
+        - increase the day number by one
+
+after while loop:
+    - calculate average: sum of temperature sum variable / 7
+    - print the result
 """
 
+day_number = 1
+temperature_sum = 0
+
+while day_number <= 7:
+    # get the temp
+    # increase day number
+
+# calculate average
+# present the results
