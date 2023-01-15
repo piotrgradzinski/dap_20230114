07a26315bea85b550391a4ee888d0ce8d865fa35
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-01-15 08:26:35 +0100

    Day 2 - ex 4.

--- day_1_pgg/ex_4.py
+++ day_1_pgg/ex_4.py
@@ -3,4 +3,20 @@ Write a program to check if the number given by the user:
 is odd, divisible by 3 and greater than 10
 or
 is the number 7
+
+Sample program output:
+Enter the number: 15
+Result: True
+
+Enter the number: 21
+Result: True
+
+Enter the number: 7
+Result: True
 """
+
+number = int(input('Enter the number'))
+
+result = number > 10 or number == 7
+
+print(result)
