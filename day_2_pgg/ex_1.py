569323261962671c05aa16f28629820120cbfbd3
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-01-15 09:58:17 +0100

    Day 2 - ex 1.

--- day_2_pgg/ex_1.py
+++ day_2_pgg/ex_1.py
@@ -5,5 +5,20 @@ Use hardcoded number for the current year.
 Sample program output:
 Enter year of birth: 1980
 You are of legal age!
+
+f(x) = x + 1
+g(y) = y * 10
+
+g(f(6)) -> 70
+g(7)
+70
 """
+
+year_of_birth = int(input('Enter year of birth: '))
+age = 2023 - year_of_birth
+
+if age >= 18:
+    print('You are of a legal age!')
+else:
+    print('You are NOT of a legal age!')
 
