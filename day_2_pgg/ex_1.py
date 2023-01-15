af77ed85164e783ea1782b0485968add98bfc28f
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-01-15 09:40:21 +0100

    Day 2 - ex 1.

---
+++ day_2_pgg/ex_1.py
@@ -0,0 +1,9 @@
+"""
+Write a program that will check the age of the person based on their year of birth.
+Use hardcoded number for the current year.
+
+Sample program output:
+Enter year of birth: 1980
+You are of legal age!
+"""
+
