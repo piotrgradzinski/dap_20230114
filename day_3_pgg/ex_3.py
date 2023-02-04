190e140ccb50e45962284ee00ef34c3cdba43f4a
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-02-04 10:01:21 +0100

    Day 3 - strings, ex 3.

--- day_3_pgg/ex_3.py
+++ day_3_pgg/ex_3.py
@@ -6,6 +6,17 @@ Write a program that counts the number of occurrences of vowels (`a, e, i, o, u,
 3. Check if particular letter is in the list of vowels, if so, count the letter.
 
 What we can do with uppercase letters?
-1. We can add the to the vowels list.
+1. We can add to the vowels list.
 2. We can use .lower() method.
 """
+
+user_input = input('Provide a string: ')
+
+vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+number_of_vowels = 0
+
+for character in user_input.lower():
+    if character in vowels:
+        number_of_vowels += 1
+
+print(f'String {user_input} contains {number_of_vowels} vowels.')
