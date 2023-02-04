f9d5a3ecd6db0f3d4a4d9b554139c88f4501d454
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-02-04 09:17:23 +0100

    Day 3 - enumerate, ex 2

--- day_3_pgg/ex_2.py
+++ day_3_pgg/ex_2.py
@@ -1,6 +1,10 @@
 """
 Write a program that will display the results of multiplication of two numbers (1-10).
 
+We should use:
+- nested for loop
+- range function
+
 Sample output:
 1*1=1
 1*2=2
@@ -9,3 +13,7 @@ Sample output:
 ...
 10*10=100
 """
+
+for a in range(1, 11):
+    for b in range(1, 11):
+        print(f'{a}*{b}={a*b}')
