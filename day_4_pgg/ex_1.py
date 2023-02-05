27cfa1396961571a64ea374806c578920b1cc8cd
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2023-02-05 08:55:56 +0100

    Day 3 - comprehensions.

--- day_4_pgg/ex_1.py
+++ day_4_pgg/ex_1.py
@@ -5,3 +5,33 @@ Create a simple, function based calculator
 3. Do the calculation and present results
 """
 
+def add(a, b):
+    return a + b
+
+def substract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+
+number_1 = float(input('Provide number 1: '))
+number_2 = float(input('Provide number 2: '))
+operation = input('Provide operation(+, -, *, /): ')
+
+if operation == '+':
+    result = add(number_1, number_2)
+elif operation == '-':
+    result = substract(number_1, number_2)
+elif operation == '*':
+    result = multiply(number_1, number_2)
+elif operation == '/':
+    result = divide(number_1, number_2)
+else:
+    print('Invalid operation.')
+    exit(1)
+
+print(f'{number_1}{operation}{number_2}={result}')
